faeb5af0d1506327616b543adad2b5db895d5dd3
paulheisele@gmail.com <paulheisele@gmail.com>
2018-05-29 00:37:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,4 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
-  elsif int % 5 == 0 #if the number int is divisible by 5
-  "Buzz"
-  end
-  elsif int % 15 == 0 #if the number int is divisible by 5
-  "FizzBuzz"
-  end
-else int !(nt % 3 == 0 || int % 5 == 0)
-  returns nil
-end
 end
